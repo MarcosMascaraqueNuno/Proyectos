6cf1169cca592901a2797f3b7f13f809380f316f
Marcos Mascaraque <marcosmk2004@gmail.com>
2023-04-21 16:20:23 +0200

    Guardando todos los archivos y carpetas de Programas

--- ejercicio4/src/ejercicio4/Ejercicio4.java
+++ ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -1,97 +1,77 @@
 package ejercicio4;
 
 public class Ejercicio4 {
-	public class SmartDevice {
-		// atributos
-		String marca;
-		String modelo;
-		
-		// contructores
-		
-		public SmartDevice() {
-			
-		}
-		
-		public SmartDevice(String marca, String modelo) {
-			this.marca = marca;
-			this.modelo = modelo;
-		}
+    public static class SmartDevice {
+        // attributes
+        String marca;
+        String modelo;
 
-		@Override
-		public String toString() {
-			return "SmartDevice [marca=" + marca + ", modelo=" + modelo + "]";
-		}
-		
-		
-		
-	}
-		
-		public class SmartPhone extends SmartDevice{
-			// atributos
-			String ram;
-			String almacenamiento;
-			
-			
-			// contructores
-			public SmartPhone() {
-				
-			}
-			// contructores
-			public SmartPhone(String marca, String modelo, String ram, String almacenamiento) {
-				super(marca, modelo);
-				this.ram = ram;
-				this.almacenamiento = almacenamiento;
-			}
-			@Override
-			public String toString() {
-				return "SmartPhone [ram=" + ram + ", almacenamiento=" + almacenamiento + ", marca=" + marca
-						+ ", modelo=" + modelo + "]";
-			}
-		
-			
-		
-		}
-		
-		
-		
-		public class SmartWatch extends SmartDevice{
-			// atributos
-			String pantalla;
-			String tamaño;
-			
-			
-			
-			// contructores
-			public SmartWatch () {
-			}
-			
+        // constructors
+        public SmartDevice() {
+        }
 
-			public SmartWatch(String marca, String modelo, String pantalla, String tamaño) {
-				super(marca, modelo);
-				this.pantalla = pantalla;
-				this.tamaño = tamaño;
-			}
+        public SmartDevice(String marca, String modelo) {
+            this.marca = marca;
+            this.modelo = modelo;
+        }
 
+        @Override
+        public String toString() {
+            return "SmartDevice [marca=" + marca + ", modelo=" + modelo + "]";
+        }
+    }
 
-			@Override
-			public String toString() {
-				return "SmartWatch [pantalla=" + pantalla + ", tamaño=" + tamaño + "]";
-			}
+    public static class SmartPhone extends SmartDevice {
+        // attributes
+        String ram;
+        String almacenamiento;
 
+        // constructors
+        public SmartPhone() {
+        }
 
-		
-		
-	}
-		
-		public class Main {
-		    public static void main(String[] args) {
-		        SmartPhone smartPhone = new SmartPhone("Samsung", "Galaxy S20", "8 GB", "128 GB");
-		        System.out.println(miSmartPhone);
+        public SmartPhone(String marca, String modelo, String ram, String almacenamiento) {
+            super(marca, modelo);
+            this.ram = ram;
+            this.almacenamiento = almacenamiento;
+        }
 
-		        SmartWatch smartWatch = new SmartWatch("Apple", "Watch Series 6", "OLED", "44 mm");
-		        System.out.println(miSmartWatch);
-		    }
-		}
+        @Override
+        public String toString() {
+            return "SmartPhone [ram=" + ram + ", almacenamiento=" + almacenamiento + ", marca=" + marca
+                    + ", modelo=" + modelo + "]";
+        }
+    }
 
+    public static class SmartWatch extends SmartDevice {
+        // attributes
+        String pantalla;
+        String tamaño;
+
+        // constructors
+        public SmartWatch() {
+        }
+
+        public SmartWatch(String marca, String modelo, String pantalla, String tamaño) {
+            super(marca, modelo);
+            this.pantalla = pantalla;
+            this.tamaño = tamaño;
+        }
+
+        @Override
+        public String toString() {
+            return "SmartWatch [pantalla=" + pantalla + ", tamaño=" + tamaño + ", marca=" + marca
+                    + ", modelo=" + modelo + "]";
+        }
+    }
+
+    public static class Main {
+        public static void main(String[] args) {
+            SmartPhone smartPhone = new SmartPhone("Samsung", "Galaxy S20", "8 GB", "128 GB");
+            System.out.println(smartPhone);
+
+            SmartWatch smartWatch = new SmartWatch("Apple", "Watch Series 6", "OLED", "44 mm");
+            System.out.println(smartWatch);
+        }
+    }
 }
-
