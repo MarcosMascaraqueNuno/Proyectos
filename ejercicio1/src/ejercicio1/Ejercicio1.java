51f78480bcf79b0f1ec6dacf58eec596481096dd
Marcos <marcosmk2004@gmail.com>
2023-04-21 12:48:46 +0200

    Guardando todos los archivos y carpetas de Programas

---
+++ ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,11 @@
+package ejercicio1;
+
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		int num = 10;
+		long bigNum = 1234567890L;
+		double decimal = 3.14;
+		boolean check = true;
+		String color = "rojo";
+	}
+}
